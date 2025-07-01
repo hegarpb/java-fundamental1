90544aae5a5561e94149ddbd997744f09200e2f2
hegarpb <hegaregayz@gmail.com>
2025-07-01 20:19:13 +0700

    test2

---
+++ src/variable.java
@@ -0,0 +1,18 @@
+public class variable {
+    public static void main(String []args){
+/*    VARIABLE */
+        byte minRequest =1;
+        short maxProduct =300;
+        int stock =99000;
+        long total =3000000000L;
+        float rating = 4.9f;
+        double saldo =2000000000.50;
+        System.out.println(minRequest);
+        System.out.println(maxProduct);
+        System.out.println(stock);
+        System.out.println(total);
+        System.out.printf("Rating: %.2f\n", rating);
+        System.out.printf("Saldo: %.2f\n", saldo);
+
+    }
+}
