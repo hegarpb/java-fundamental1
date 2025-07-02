fb2eebed2a55716b5450882957f83cc7bd4daf9d
hegarpb <hegaregayz@gmail.com>
2025-07-02 21:45:05 +0700

    ex

---
+++ src/operator.java
@@ -0,0 +1,7 @@
+public class operator {
+    public static void main(String [] args ){
+int a=20;
+    a+=5;
+        System.out.println(a);
+    }
+}
---
+++ src/percabangan.java
@@ -0,0 +1,13 @@
+public class percabangan {
+    public static void main(String [] args){
+        String ekspedisi="Kargo";
+        int harga;
+
+        if (ekspedisi=="JNE") {
+            harga = 28000;
+        } else if (ekspedisi=="Kargo"){
+            harga=30000;
+        }
+
+    }
+}
