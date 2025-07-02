cd9ec63329a7cfd56330f0fd595595cb36184eca
hegarpb <hegaregayz@gmail.com>
2025-07-02 19:19:22 +0700

    ex

--- src/variable.java
+++ src/variable.java
@@ -1,18 +1,11 @@
 public class variable {
     public static void main(String []args){
 /*    VARIABLE */
-        byte minRequest =1;
-        short maxProduct =300;
-        int stock =99000;
-        long total =3000000000L;
-        float rating = 4.9f;
-        double saldo =2000000000.50;
-        System.out.println(minRequest);
-        System.out.println(maxProduct);
-        System.out.println(stock);
-        System.out.println(total);
-        System.out.printf("Rating: %.2f\n", rating);
-        System.out.printf("Saldo: %.2f\n", saldo);
-
+     int price =10000;
+     float diskon=2.5f;
+     float total=(diskon/100)*price;
+     double bayar=price-total;
+        System.out.printf("Diskon = "+"%.2f\n",total);
+        System.out.printf("Harga "+"%.2f\n",bayar);
     }
 }
